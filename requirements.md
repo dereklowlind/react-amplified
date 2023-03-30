fa9193b5fde47bc0752bc10e8dea79a1375a8584
dereklowlind <38629827+dereklowlind@users.noreply.github.com>
2023-03-30 13:40:13 -0400

    first commit

---
+++ requirements.md
@@ -0,0 +1,9 @@
+Use the amplify DataStore library to manage the todos.
+    - Use DataStore Subscriptions to manage real-time updates.
+    - Datastore is enabled with conflict detection.
+Add the ability to add a "Finished" state to todos that is persisted to the database. -- done
+    - Create UI to filter todos by: All | Unfinished | Finished
+Add the ability to add a "Category" to todos that is persisted to the database. -- done
+    - Create UI to filter todos by Category
+    - Add the ability to add a "Category" to todos that is persisted to the database.
+Add the project to a github (or other) repository
--- src/App.js
+++ src/App.js
@@ -1,25 +1,211 @@
-import logo from './logo.svg';
-import './App.css';
+/* src/App.js */
+import React, { useEffect, useState } from "react";
+import { Amplify, API, graphqlOperation } from "aws-amplify";
+import { createTodo, updateTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
+import { onCreateTodo, onUpdateTodo } from "./graphql/subscriptions";
+// import { Todo } from '../amplify/backend/api/executivehomes';
 
-function App() {
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+
+const initialState = { name: "", description: "", category: "" };
+
+const App = () => {
+  const [formState, setFormState] = useState(initialState);
+  const [todos, setTodos] = useState([]);
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  const [categoryFilter, setCategoryFilter] = useState("all");
+  const [finishedFilter, setFinishedFilter] = useState("all");
+  const [possibleCategories, setPossibleCategories] = useState({});
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  useEffect(() => {
+    const createSubscription = API.graphql(graphqlOperation(onCreateTodo)).subscribe((msg) => {
+      console.log(msg);
+      const createdTodo = msg.value.data.onCreateTodo;
+      // console.log("🚀 ~ file: App.js:31 ~ createSubscription ~ createdTodo:", createdTodo)
+      setTodos([...todos, createdTodo]);
+    });
+
+    const updateSubscription = API.graphql(graphqlOperation(onUpdateTodo)).subscribe((msg) => {
+      const updatedTodo = msg.value.data.onUpdateTodo;
+      // console.log("🚀 ~ file: App.js:38 ~ updateSubscription ~ updatedTodo:", updatedTodo);
+      for(let i = 0; i<todos.length; i++){
+        if(todos[i].id === updatedTodo.id){
+          todos[i] = updatedTodo;
+          setTodos([...todos]);
+        }
+      }
+    });
+
+    return () => {
+      createSubscription.unsubscribe();
+      updateSubscription.unsubscribe();
+    }
+  }, []); 
+  // useEffect(() => {
+  //   const subscription = DataStore.observe(Todo).subscribe((msg) => {
+  //     console.log(msg.model, msg.opType, msg.element);
+  //   });
+
+  //   return () => subscription.unsubscribe();
+  // }, []);
+
+  useEffect(() => {
+    let filtered = todos;
+    if (categoryFilter !== "all") {
+      filtered = filtered.filter((todo) => {
+        return todo.category === categoryFilter;
+      });
+    }
+
+    if(finishedFilter !== 'all'){
+      const finishedbool = (finishedFilter === 'finished'); 
+      filtered = filtered.filter((todo) => {
+        return todo.finished === finishedbool;
+      });
+    }
+    setFilteredTodos(filtered);
+  }, [todos, categoryFilter, finishedFilter]);
+
+  function setInput(key, value) {
+    setFormState({ ...formState, [key]: value });
+  }
+
+  async function fetchTodos() {
+    try {
+      const todoData = await API.graphql(graphqlOperation(listTodos));
+      const todos = todoData.data.listTodos.items;
+      let possibleCategories = {"all":true};
+      todos.forEach((t) => {
+        if (t.category) {
+          possibleCategories[t.category] = true;
+        }
+      });
+      setPossibleCategories(possibleCategories);
+      console.log("🚀 ~ file: App.js:28 ~ fetchTodos ~ todos:", todos);
+      setTodos(todos);
+    } catch (err) {
+      console.log("error fetching todos", err);
+    }
+  }
+
+  async function addTodo() {
+    try {
+      if (!formState.name || !formState.description) return;
+      const todo = { ...formState, finished: false };
+      // setTodos([...todos, todo]);
+      // add category
+      setFormState(initialState);
+      await API.graphql(graphqlOperation(createTodo, { input: todo }));
+    } catch (err) {
+      console.log("error creating todo:", err);
+    }
+  }
+
+  async function handleFinishChange(event, todo){
+    console.log("🚀 ~ file: App.js:130 ~ handleFinishChange ~ event.target.checked:", event.target.checked)
+    const newDetails = {id: todo.id, finished: event.target.checked};
+    
+    try {
+      await API.graphql(graphqlOperation(updateTodo, { input: newDetails }));
+    } catch (err) {
+      console.log("error updating todo:", err);
+    }
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={styles.container}>
+      <h2>Todos App</h2>
+      <input
+        onChange={(event) => setInput("name", event.target.value)}
+        style={styles.input}
+        value={formState.name}
+        placeholder="Name"
+      />
+      <input
+        onChange={(event) => setInput("description", event.target.value)}
+        style={styles.input}
+        value={formState.description}
+        placeholder="Description"
+      />
+      <input
+        onChange={(event) => setInput("category", event.target.value)}
+        style={styles.input}
+        value={formState.category}
+        placeholder="Category"
+      />
+      <button style={styles.button} onClick={addTodo}>
+        Create Todo
+      </button>
+      <label>
+        Category Filter
+        <select
+          value={categoryFilter}
+          onChange={(e) => setCategoryFilter(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          {Object.keys(possibleCategories).map((c) => (
+            <option value={c}>{c}</option>
+          ))}
+        </select>
+      </label>
+      <label>
+        Finished Filter
+        <select
+          value={finishedFilter}
+          onChange={(e) => setFinishedFilter(e.target.value)}
+        >
+          <option value={"all"}>All</option>
+          <option value={"unfinished"}>Unfinished</option>
+          <option value={"finished"}>Finished</option>
+        </select>
+      </label>
+      {filteredTodos.map((todo, index) => (
+        <div key={todo.id ? todo.id : index} style={styles.todo}>
+          <p style={styles.todoName}>{todo.name}</p>
+          <p style={styles.todoDescription}>Description: {todo.description}</p>
+          <p style={styles.todoCategory}>Category: {todo.category}</p>
+          <label>
+            Finished <input type="checkbox" checked={todo.finished} onChange={e => handleFinishChange(e, todo)}/>
+          </label>
+        </div>
+      ))}
     </div>
   );
-}
+};
+
+const styles = {
+  container: {
+    width: 400,
+    margin: "0 auto",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: 20,
+  },
+  todo: { marginBottom: 15 },
+  input: {
+    border: "none",
+    backgroundColor: "#ddd",
+    marginBottom: 10,
+    padding: 8,
+    fontSize: 18,
+  },
+  todoName: { fontSize: 20, fontWeight: "bold" },
+  todoDescription: { marginBottom: 0 },
+  button: {
+    backgroundColor: "black",
+    color: "white",
+    outline: "none",
+    fontSize: 18,
+    padding: "12px 0px",
+  },
+};
 
 export default App;
